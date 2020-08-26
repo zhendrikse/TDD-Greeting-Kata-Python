ee7e41c07b3dd7d06d95a1c6dedef643849cac6c
Zeger Hendrikse <zegerh@yahoo.co.uk>
2020-08-26 13:22:10 +0000

    Made test method names Python compliant

--- test_main.py
+++ test_main.py
@@ -1,31 +1,31 @@
 from main import GreetingMessageGenerator
 
-def testGreetVisitorSimpleHelloPositiveCase():
+def test_greet_visitor_simple_hello_positive_case():
     assert GreetingMessageGenerator().greet_visitor("Bob") == "Hello, Bob."
 
-def testGreetVisitorNoneAsInput():
+def test_greet_visitor_none_as_input():
     assert GreetingMessageGenerator().greet_visitor(None) == "Hello, my friend."
 
-def testGreetVisitorEmptyNameAsInput():
+def test_greet_visitor_empty_name_as_input():
     assert GreetingMessageGenerator().greet_visitor("") == "Hello, my friend."
 
-def testGreetVisitorShouting():
+def test_greet_visitor_shouting():
     assert GreetingMessageGenerator().greet_visitor("BOB") == "HELLO BOB!"
 
-def testGreetTwoVisitors():
+def test_greet_two_visitors():
     assert GreetingMessageGenerator().greet_visitor("Jill", "Jane") == "Hello, Jill and Jane."
 
-def testGreetTwoVisitorsFirstOneNone():
+def test_greet_two_visitors_first_one_none():
     assert GreetingMessageGenerator().greet_visitor(None, "Jane") == "Hello, my friend and Jane."
 
-def testGreetTwoVisitorsSecondtOneNone():
+def test_greet_two_visitors_second_one_none():
     assert GreetingMessageGenerator().greet_visitor("Jill", None) == "Hello, Jill and my friend."
 
-def testGreetTwoVisitorsFirstOneEmpty():
+def test_greet_two_visitors_first_one_empty():
     assert GreetingMessageGenerator().greet_visitor("", "Jane") == "Hello, my friend and Jane."
 
-def testGreetThreeVisitors():
+def test_greet_three_visitors():
     assert GreetingMessageGenerator().greet_visitor("Amy", "Brian", "Charlotte") == "Hello, Amy, Brian, and Charlotte."
 
-def testGreetThreeVisitorsOneShouting():
+def test_greet_three_visitors_one_shouting():
     assert GreetingMessageGenerator().greet_visitor("Amy", "BRIAN", "Charlotte") == "Hello, Amy and Charlotte. AND HELLO BRIAN!"
