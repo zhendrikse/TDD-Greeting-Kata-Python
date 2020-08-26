c3b043e842a771270d9b5d6081b71b8e3dd504f6
Zeger Hendrikse <zegerh@yahoo.co.uk>
2020-08-26 13:18:14 +0000

    Created class to house functions.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 
+class GreetingMessageGenerator:
 
-def greet_visitor(name: str, *names: [str]) -> str:
+  def greet_visitor(self, name: str, *names: [str]) -> str:
     visitor: str = name
     if name is None:
         visitor = "my friend"
@@ -9,17 +10,17 @@ def greet_visitor(name: str, *names: [str]) -> str:
         visitor = "my friend"
 
     if visitor.isupper():
-        return "HELLO " + _create_visitor_list(visitor, *names) + "!"
+        return f"HELLO {self._create_visitor_list(visitor, *names)}!"
     else:
-        return "Hello, " + _create_visitor_list(visitor, *names) + "."
+        return f"Hello, {self._create_visitor_list(visitor, *names)}."
 
 
-def _two_names_list(name1: str, name2: str) -> str:
+  def _two_names_list(self, name1: str, name2: str) -> str:
     other: str = "my friend" if name2 is None else name2
     return f"{name1} and {other}"
 
 
-def _multiple_names_list(name: str, *names) -> str:
+  def _multiple_names_list(self, name: str, *names) -> str:
     names_list: str = name
     for i in range(len(names)):
         separator: str = ", " if i < len(names) - 1 else ", and "
@@ -28,12 +29,12 @@ def _multiple_names_list(name: str, *names) -> str:
     return names_list
 
 
-def _create_visitor_list(name: str, *names: [str]) -> str:
+  def _create_visitor_list(self, name: str, *names: [str]) -> str:
     if names:
         if len(names) == 1:
-            return _two_names_list(name, names[0])
+            return self._two_names_list(name, names[0])
         else:
-            return _multiple_names_list(name, *names)
+            return self._multiple_names_list(name, *names)
 
     return name
 
--- test_main.py
+++ test_main.py
@@ -1,31 +1,31 @@
-from main import greet_visitor
+from main import GreetingMessageGenerator
 
 def testGreetVisitorSimpleHelloPositiveCase():
-    assert greet_visitor("Bob") == "Hello, Bob."
+    assert GreetingMessageGenerator().greet_visitor("Bob") == "Hello, Bob."
 
 def testGreetVisitorNoneAsInput():
-    assert greet_visitor(None) == "Hello, my friend."
+    assert GreetingMessageGenerator().greet_visitor(None) == "Hello, my friend."
 
 def testGreetVisitorEmptyNameAsInput():
-    assert greet_visitor("") == "Hello, my friend."
+    assert GreetingMessageGenerator().greet_visitor("") == "Hello, my friend."
 
 def testGreetVisitorShouting():
-    assert greet_visitor("BOB") == "HELLO BOB!"
+    assert GreetingMessageGenerator().greet_visitor("BOB") == "HELLO BOB!"
 
 def testGreetTwoVisitors():
-    assert greet_visitor("Jill", "Jane") == "Hello, Jill and Jane."
+    assert GreetingMessageGenerator().greet_visitor("Jill", "Jane") == "Hello, Jill and Jane."
 
 def testGreetTwoVisitorsFirstOneNone():
-    assert greet_visitor(None, "Jane") == "Hello, my friend and Jane."
+    assert GreetingMessageGenerator().greet_visitor(None, "Jane") == "Hello, my friend and Jane."
 
 def testGreetTwoVisitorsSecondtOneNone():
-    assert greet_visitor("Jill", None) == "Hello, Jill and my friend."
+    assert GreetingMessageGenerator().greet_visitor("Jill", None) == "Hello, Jill and my friend."
 
 def testGreetTwoVisitorsFirstOneEmpty():
-    assert greet_visitor("", "Jane") == "Hello, my friend and Jane."
+    assert GreetingMessageGenerator().greet_visitor("", "Jane") == "Hello, my friend and Jane."
 
 def testGreetThreeVisitors():
-    assert greet_visitor("Amy", "Brian", "Charlotte") == "Hello, Amy, Brian, and Charlotte."
+    assert GreetingMessageGenerator().greet_visitor("Amy", "Brian", "Charlotte") == "Hello, Amy, Brian, and Charlotte."
 
 def testGreetThreeVisitorsOneShouting():
-    assert greet_visitor("Amy", "BRIAN", "Charlotte") == "Hello, Amy and Charlotte. AND HELLO BRIAN!"
+    assert GreetingMessageGenerator().greet_visitor("Amy", "BRIAN", "Charlotte") == "Hello, Amy and Charlotte. AND HELLO BRIAN!"
